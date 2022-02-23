5c1948737414b1a06d36d060b4f925f17134333b
Tiago Matos <tiago@tiagomatos.com>
2022-02-23 15:08:39 +1100

    aula-9

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,41 +1,17 @@
+@extends('layouts.default')
 
+@section('title', 'User title')
 
+@section('sidebar')
+    <div>
+        <nav>
+            Sidebar de user
+        </nav>
+    </div>
+@endsection
+
+@section('content')
 <h1>User</h1>
 {{ $user->name }} <br>
 {{ $user->email }} <br>
-
-<br>
-{{ date('d/m/y') }}
-
-<br>
-@php
-    $total = 0;
-    $array = [];
-    $boolean = false;
-@endphp
-
-Unless<br>
-@unless($boolean)
-    Falso
-@endunless
-<br><br>
-
-If<br>
-@if($boolean)
-    True
-@endif
-<br><br>
-
-@empty($array)
-    O array está vazio jovem!
-@endempty
-
-<br><br>
-
-@if($total > 100)
-    Muito caro
-@elseif($total > 80)
-    tá ok
-@elseif($total > 50)
-    tá barato
-@endif
+@endsection

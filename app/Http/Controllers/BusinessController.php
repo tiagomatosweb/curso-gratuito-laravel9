a49f5a93b1bb7321043ad7b3f7ae1a243fac0271
Tiago Matos <tiago@tiagomatos.com>
2022-02-28 14:34:32 +1100

    aula-18

--- app/Http/Controllers/BusinessController.php
+++ app/Http/Controllers/BusinessController.php
@@ -9,24 +9,11 @@ class BusinessController extends Controller
 {
     public function index()
     {
-        // $business = Business::find(10)->update([
-        //     'name' => 'Jon',
-        //     'email' => 'sdsd@sdsd.com'
-        // ]);
-        //
-        // $business = Business::find(10);
-        // $business->name = 'Tiago';
-        // $business->email = 'tiago@laravel9.com';
-        // $business->address = 'Quadra c rua B';
-        // $business->save();
-
-        $input = [
-            'name' => 'Jon 2',
-            'email' => 'sdsd@sdsd.com2',
-        ];
-        $business = Business::find(10);
-        $business->fill($input);
-        $business->save();
-        dd($business);
+        \DB::connection()->enableQueryLog();
+        $business = Business::where('name', 'LIKE', '%jon%')->get();
+        $query = \DB::getQueryLog();
+        dd($query);
+        // $business->delete();
+        // dd($business);
     }
 }
